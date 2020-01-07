f4e7ab7dfc179d6b8a3bf65a9eca7952b775f0b2
Lian Yelich <lianyelich14@gmail.com>
2020-01-07 22:46:23 +0000

    Added the assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  correctCount = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      correctCount++;
+    }
+    if (correctCount === arr1.length){
+      return true;
+    }
+  } 
+  return false;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`)
+  }
+  if (!(eqArrays(arr1, arr2))) {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+}
+
+assertArraysEqual(["1", "2", "3"],["1", "2", "3"]);
