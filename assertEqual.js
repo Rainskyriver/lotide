1f66d6ee02bafbb57d6ccd785944c2f1cf962065
Lian Yelich <lianyelich14@gmail.com>
2020-01-08 20:41:15 +0000

    Added the countOnly function

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,3 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Gooby", "Gooby");
