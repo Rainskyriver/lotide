628237dd1d0c987135910d08b31ed405a11bebd5
Lian Yelich <lianyelich14@gmail.com>
2020-01-08 20:59:13 +0000

    Added the countLetters function

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const convertToArray = function(str) {
+    let storeArray = str.split(' ').join('');
+    return storeArray;
+  };
+  letters = convertToArray(string);
+  results = {};
+  for (const letter of letters) {
+    if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+    }
+  }
+  return results;
+}
+
+const result1 = countLetters("Lighthouse in the houseAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzbgfxnvseathtrh");
+console.log(result1);
