a03c568180ef052a239fe447bffa43933b95a1ef
Lian Yelich <lianyelich14@gmail.com>
2020-01-08 21:39:42 +0000

    Added the findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(keys, val) { 
+  let results = ""
+    for (const key in keys) {
+      if (keys[key] === val) {
+        results = key;
+      }
+    }
+  return results;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
