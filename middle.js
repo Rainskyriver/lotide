8e0779aa68d8d89b2f73f98d4ad1ab64b86642f1
Lian Yelich <lianyelich14@gmail.com>
2020-01-13 20:27:25 +0000

    Refactored middle.js

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  let correctCount = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      correctCount++;
-    }
-    if (correctCount === arr1.length) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  }
-  if (!(eqArrays(arr1, arr2))) {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(arr) {
   let midArray = [];
@@ -33,5 +14,4 @@ const middle = function(arr) {
   return midArray;
 };
 
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
